533771cf7a6cae3ae742cf4118e6ac5c7bee8afd
Benjamin Reid <benjamin@nouveller.com>
2015-07-08 19:43:24 +0100

    Remove whitespace from gulpfile

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
-<% if (sass) { %>var sass = require('gulp-sass');<% } %>
+<% if (sass) { -%>
+var sass = require('gulp-sass');
+<% } -%>
 
 gulp.task('default', function() {
   console.log('Yo! This is just a plan ol\' default task');
